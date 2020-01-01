8c7b9c83ca54a96b4e6a4b1c33a6085730b162cf
Raul de Melo <melo.raulf@gmail.com>
2020-01-01 16:50:00 +0100

    fix response object for root

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { fetchEpisodes } from "./services/episodes";
 const routes = async () => {
   const service = await OpenSubtitles;
 
-  app.get("/", async (res) => {
+  app.get("/", async (req, res) => {
     res.json({ health: "ok" });
   });
 
