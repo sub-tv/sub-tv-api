531ac543db7f673c8854ca2a8446a2f42e10040d
Raul de Melo <melo.raulf@gmail.com>
2020-01-12 14:44:32 +0100

    refactory entry file

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import { config } from "dotenv";
+config();
+import pino from "express-pino-logger";
+import express, { NextFunction, Response, Request } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import apiRoutes from "./routes/api";
+
+const port = 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(pino());
+app.use(bodyParser.json());
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
+
+app.use("/api", apiRoutes);
+
+app.use("/", async (_, res) => {
+  res.json({ health: "ok" });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
